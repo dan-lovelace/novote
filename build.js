c1f97bcdca4eb234537e2656e0e710ecdbf03693
devdan118 <dlovelace085@gmail.com>
2018-07-22 17:05:50 -0600

    added js zip

--- build.js
+++ build.js
@@ -1,10 +1,19 @@
 const fs = require('fs');
+const admZip = require('adm-zip');
 
 const VERSION = '@VERSION@';
 
-const manifest = fs.readFile('./src/manifest.json', function(err, data) {
+// update extension's manifest file
+const manifest = fs.readFile('./src/manifest.json', function(readErr, data) {
+  if (readErr) return;
   const json = JSON.parse(data);
 
   json.version = VERSION;
-  fs.writeFile('./src/manifest.json', JSON.stringify(json, null, 2));
+  fs.writeFile('./src/manifest.json', JSON.stringify(json, null, 2), function(writeErr) {
+    if (writeErr) return;
+
+    // zip it all up with the latest build
+    admZip.addLocalFolder('src');
+    zip.writeZip('./build/' + VERSION + '.zip');
+  });
 });
