aee3ae6698f6475187627cd352a880bd8a39c08e
devdan118 <dlovelace085@gmail.com>
2018-07-21 18:38:40 -0600

    updated name, version bump

--- reddit.js
+++ reddit.js
@@ -1,8 +1,11 @@
 function removeButtons() {
-  const buttons = document.querySelectorAll('div:not(.Comment) > div > [aria-label="upvote"]');
+  // TODO: add option to allow comment votes
+  // const buttons = document.querySelectorAll('div:not(.Comment) > div > [aria-label="upvote"]');
+
+  const buttons = document.querySelectorAll('[aria-label="upvote"]');
 
   Array.prototype.forEach.call(buttons, function(e, i) {
-    e.parentNode.style = 'display: none';
+    e.parentNode.innerHTML = '&nbsp;';
   });
 }
 
