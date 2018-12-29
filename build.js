b22111ef0398d734c8d3160f8ca30b65a40b3d79
devdan118 <dlovelace085@gmail.com>
2018-12-29 15:56:05 -0700

    updated github repo

--- build.js
+++ build.js
@@ -3,12 +3,12 @@ const fs = require('fs');
 const VERSION = '@VERSION@';
 
 // update extension's manifest file
-const manifest = fs.readFile('./src/manifest.json', function(readErr, data) {
+const manifest = fs.readFile('./src/manifest.json', (readErr, data) => {
   if (readErr) return;
   const json = JSON.parse(data);
 
   json.version = VERSION;
-  fs.writeFile('./src/manifest.json', JSON.stringify(json, null, 2), function(writeErr) {
+  fs.writeFile('./src/manifest.json', JSON.stringify(json, null, 2), writeErr => {
     if (writeErr) return;
 
     // zip it all up with the latest build
--- src/background.js
+++ src/background.js
@@ -1,7 +1,5 @@
-'use strict';
-
-chrome.runtime.onInstalled.addListener(function() {
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
     chrome.declarativeContent.onPageChanged.addRules([
       {
         conditions: [
