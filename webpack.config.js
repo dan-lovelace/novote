83903432ced1b52cf4634ad0a3171be8a2fe0866
Top Notch <topnotch@Tops-MacBook-Pro-2.local>
2018-12-30 00:06:09 -0700

    new release: 0.1.46

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,8 @@ module.exports = {
     }),
     new HtmlWebpackPlugin({
       template: './src/index.html',
-      inject: true
+      filename: 'index.html',
+      inject: false
     })
   ],
   devServer: {
