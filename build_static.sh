4c73d60ed4ce346c3c990be6bfcdba06f19479eb
Top Notch <topnotch@Tops-MacBook-Pro-2.local>
2018-12-29 18:18:04 -0700

    tweaked build script

--- build_static.sh
+++ build_static.sh
@@ -5,7 +5,7 @@ mkdir -p build
 
 VERSION=$npm_package_version
 
-echo "Deploying version: " + VERSION
+echo "Deploying version: ${VERSION}"
 
 sed -i bak -e "s|@VERSION@|${VERSION}|g" build.js
 node build.js
