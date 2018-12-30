74153bf41b63710af0f44dcc904d721e22c7043c
Top Notch <topnotch@Tops-MacBook-Pro-2.local>
2018-12-29 21:42:33 -0700

    fixed build script

--- scripts/build_static.sh
+++ scripts/build_static.sh
@@ -4,11 +4,12 @@
 set -e
 
 # update package version and create the build directory
-# npm version patch
+npm version patch
 mkdir -p ./build
 
 # update zip.js with new version and run it
 VERSION="$(node -p "require('./package.json').version")"
+echo "${VERSION}"
 sed -i bak -e "s|@VERSION@|${VERSION}|g" ./scripts/zip.js
 node ./scripts/zip.js
 
