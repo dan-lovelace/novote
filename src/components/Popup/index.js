d6c92763f564a2d46240e478e0bbfe5d07577587
Top Notch <topnotch@Tops-MacBook-Pro-2.local>
2018-12-30 00:21:34 -0700

    new release: 0.1.46

--- src/components/Popup/index.js
+++ src/components/Popup/index.js
@@ -1,18 +1,12 @@
 import React from 'react';
-import { Form, FormGroup, Label, Input } from 'reactstrap';
 
 const Popup = props => (
   <div className='popup'>
     <div className='popup--content'>
       <h3>Options</h3>
-      <Form>
-        <FormGroup check>
-          <Label check>
-            <Input type='checkbox' />{' '}
-            Option 1
-          </Label>
-        </FormGroup>
-      </Form>
+      <form>
+        <input type='checkbox' /> Test
+      </form>
     </div>
   </div>
 );
