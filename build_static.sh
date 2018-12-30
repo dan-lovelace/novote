082829b276b1efa8c1a437483a2898c36410c772
Top Notch <topnotch@Tops-MacBook-Pro-2.local>
2018-12-29 18:27:09 -0700

    build script tweak

--- build_static.sh
+++ build_static.sh
@@ -2,13 +2,11 @@
 
 mkdir -p build
 
-VERSION=$npm_package_version
+echo "Deploying version: ${npm_package_version}"
 
-echo "Deploying version: ${VERSION}"
-
-sed -i bak -e "s|@VERSION@|${VERSION}|g" build.js
+sed -i bak -e "s|@VERSION@|${npm_package_version}|g" build.js
 node build.js
-sed -i bak -e "s|${VERSION}|@VERSION@|g" build.js
+sed -i bak -e "s|${npm_package_version}|@VERSION@|g" build.js
 
 git add .
 git commit -m 'version bump'
