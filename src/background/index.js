37b4701b20fdd03269fd4462f91dabad1051a3da
Top Notch <topnotch@Tops-MacBook-Pro-2.local>
2018-12-30 20:14:12 -0700

    moved command script order

--- src/background/index.js
+++ src/background/index.js
@@ -1,25 +1,3 @@
-// Enable extension on reddit.com only
-
-// chrome.runtime.onInstalled.addListener(() => {
-//   console.log('CHROME RUNTIME INSTALLED -------');
-//   chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
-//     chrome.declarativeContent.onPageChanged.addRules([
-//       {
-//         conditions: [
-//           new chrome.declarativeContent.PageStateMatcher({
-//             pageUrl: {
-//               hostContains: 'reddit.com'
-//             },
-//           })
-//         ],
-//         actions: [
-//           new chrome.declarativeContent.ShowPageAction()
-//         ]
-//       }
-//     ]);
-//   });
-// });
-
 chrome.runtime.onMessage.addListener(function (msg, sender) {
   // First, validate the message's structure
   if ((msg.from === 'content') && (msg.subject === 'showPageAction')) {
