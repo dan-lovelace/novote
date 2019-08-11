1a10209d75b44367fab8f151bc4a74f147a0b496
unknown <dlovelace085@gmail.com>
2019-08-11 10:51:12 -0600

    webpack updates

--- popup/webpack.dev.config.js
+++ popup/webpack.dev.config.js
@@ -6,11 +6,11 @@ const webpack = require('webpack');
 module.exports = {
   entry: [
     'webpack-dev-server/client?http://localhost:8080',
-    './src/index.js'
+    './src/index.js',
   ],
   output: {
     filename: 'bundle.popup.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
@@ -19,13 +19,13 @@ module.exports = {
     }),
     new HtmlWebpackPlugin({
       template: './src/public/index.html',
-      filename: 'index.html'
+      filename: 'index.html',
     }),
   ],
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
     hot: true,
-    disableHostCheck: true
+    disableHostCheck: true,
   },
   module: {
     rules: [
@@ -34,51 +34,13 @@ module.exports = {
         exclude: /node_modules/,
         loader: 'babel-loader',
         options: {
-          presets: ['babel-preset-env', 'react', 'stage-2']
-        }
+          presets: ['babel-preset-env', 'react', 'stage-2'],
+        },
       },
       {
-        test: /\.(scss|css)$/,
-        use: [
-          {
-            // creates style nodes from JS strings
-            loader: "style-loader",
-            options: {
-              sourceMap: true
-            }
-          },
-          {
-            // translates CSS into CommonJS
-            loader: "css-loader",
-            options: {
-              sourceMap: true
-            }
-          },
-          {
-            // compiles Sass to CSS
-            loader: "sass-loader",
-            options: {
-              outputStyle: 'expanded',
-              sourceMap: true,
-              sourceMapContents: true
-            }
-          }
-        ]
+        test: /\.(scss|css)$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
       },
-      {
-        // Load all images as base64 encoding if they are smaller than 8192 bytes
-        test: /\.(png|jpg|gif)$/,
-        use: [
-          {
-            loader: 'url-loader',
-            options: {
-              // On development we want to see where the file is coming from, hence we preserve the [path]
-              name: '[path][name].[ext]?hash=[hash:20]',
-              limit: 8192
-            }
-          }
-        ]
-      }
     ],
   },
 };
--- popup/webpack.prod.config.js
+++ popup/webpack.prod.config.js
@@ -4,11 +4,11 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: {
-    popup: './src/index.js'
+    popup: './src/index.js',
   },
   output: {
     filename: 'bundle.[name].js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
   },
   module: {
     rules: [
@@ -17,57 +17,19 @@ module.exports = {
         exclude: /node_modules/,
         loader: 'babel-loader',
         options: {
-          presets: ['babel-preset-env', 'react', 'stage-2']
-        }
+          presets: ['babel-preset-env', 'react', 'stage-2'],
+        },
       },
       {
-        test: /\.(scss|css)$/,
-        use: [
-          {
-            // creates style nodes from JS strings
-            loader: "style-loader",
-            options: {
-              sourceMap: true
-            }
-          },
-          {
-            // translates CSS into CommonJS
-            loader: "css-loader",
-            options: {
-              sourceMap: true
-            }
-          },
-          {
-            // compiles Sass to CSS
-            loader: "sass-loader",
-            options: {
-              outputStyle: 'expanded',
-              sourceMap: true,
-              sourceMapContents: true
-            }
-          }
-        ]
+        test: /\.(scss|css)$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
       },
-      {
-        // Load all images as base64 encoding if they are smaller than 8192 bytes
-        test: /\.(png|jpg|gif)$/,
-        use: [
-          {
-            loader: 'url-loader',
-            options: {
-              // On development we want to see where the file is coming from, hence we preserve the [path]
-              name: '[path][name].[ext]?hash=[hash:20]',
-              limit: 8192
-            }
-          }
-        ]
-      }
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/public/index.html',
-      filename: 'index.html'
-    })
-  ]
+      filename: 'index.html',
+    }),
+  ],
 };
