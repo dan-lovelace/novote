8f0888d8a3b0a65cfcafdc11b2a4595f011f272d
dan-lovelace <dlovelace085@gmail.com>
2022-01-01 10:39:09 -0700

    fix header karma, add vote percentage to post score

--- core/src/content_scripts/reddit/functions/karma.js
+++ core/src/content_scripts/reddit/functions/karma.js
@@ -6,7 +6,7 @@ export const removeKarma = () => {
 
   // catch-all
   const karmaSpans = document.querySelectorAll('div[class*="karma"], span[class*="karma"]');
-  Array.prototype.forEach.call(karmaSpans, e => {
+  Array.prototype.forEach.call(karmaSpans, (e) => {
     e.style = noDisplay;
   });
 
@@ -16,16 +16,16 @@ export const removeKarma = () => {
 
   // header if logged in
   try {
-    const currentUserInfo = document.querySelector('.header-user-dropdown img').nextSibling;
-    const currentUserKarma = currentUserInfo.querySelector('span:nth-child(2)');
-    currentUserKarma.style = noDisplay;
+    const karmaWrapper = document.querySelector('.header-user-dropdown .icon-karma_fill').parentNode;
+    karmaWrapper.style = noDisplay;
   } catch (e) {
     // assume not logged in
   }
 
   // user profile page if displayed
   try {
-    const userProfileKarma = document.querySelector('span#profile--id-card--highlight-tooltip--karma').parentNode.parentNode;
+    const userProfileKarma = document.querySelector('span#profile--id-card--highlight-tooltip--karma').parentNode
+      .parentNode;
     userProfileKarma.style = noDisplay;
 
     if (userProfileKarma && userProfileKarma.previousSibling) {
@@ -38,7 +38,7 @@ export const removeKarma = () => {
   // hovering a user's name
   try {
     const tooltipDivs = document.querySelectorAll('div[id*="UserInfoTooltip"] div > div > div');
-    Array.prototype.forEach.call(tooltipDivs, e => {
+    Array.prototype.forEach.call(tooltipDivs, (e) => {
       if (e.innerHTML.toLowerCase().indexOf('karma') >= 0) {
         e.parentNode.style = noDisplay;
       }
--- core/src/content_scripts/reddit/functions/postScore.js
+++ core/src/content_scripts/reddit/functions/postScore.js
@@ -2,23 +2,20 @@ export const removePostScore = () => {
   const { noDisplay } = require('../index.js');
   const { hasCommentParent } = require('./commentScore.js');
 
-
   // ---------------------------------------------------------------------------
   // old
   // ---------------------------------------------------------------------------
 
   try {
-    let buttons = document.querySelectorAll('.score.unvoted');
-    Array.prototype.forEach.call(buttons, e => {
-      hasCommentParent(e, res => {
+    const buttons = document.querySelectorAll('.score.unvoted');
+    Array.prototype.forEach.call(buttons, (e) => {
+      hasCommentParent(e, (res) => {
         if (!res && e.parentNode.className !== 'tagline') {
           e.style = noDisplay;
         }
       });
     });
-  } catch (e) {
-
-  }
+  } catch (e) {}
 
   // ---------------------------------------------------------------------------
   // new
@@ -26,15 +23,21 @@ export const removePostScore = () => {
 
   // post list and post details
   try {
-    let buttons = document.querySelectorAll('[aria-label="upvote"]');
-    Array.prototype.forEach.call(buttons, e => {
-      hasCommentParent(e, res => {
+    const buttons = document.querySelectorAll('[aria-label="upvote"]');
+    Array.prototype.forEach.call(buttons, (e) => {
+      hasCommentParent(e, (res) => {
         if (!res && e.nextSibling.getAttribute('aria-label') !== 'downvote') {
           e.nextSibling.innerHTML = '&nbsp;';
         }
       });
     });
-  } catch (e) {
+  } catch (e) {}
 
-  }
+  // post details upvote percentage
+  try {
+    const postContent = document.querySelector('[data-test-id="post-content"]');
+    const postContentToolbar = postContent.lastElementChild;
+    const upvotePercent = postContentToolbar.lastChild;
+    upvotePercent.style = noDisplay;
+  } catch (e) {}
 };
